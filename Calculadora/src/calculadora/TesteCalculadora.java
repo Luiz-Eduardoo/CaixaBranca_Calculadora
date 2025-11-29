304e1959db1c8dc67470b4b800eed003c0b3fe83
Luiz-Eduardoo <luizevp4@outlook.com>
2025-11-28 21:36:35 -0300

    Refatoração

--- Calculadora/src/calculadora/TesteCalculadora.java
+++ Calculadora/src/calculadora/TesteCalculadora.java
@@ -1,3 +1,22 @@
+/**
+ * <p><strong>Projeto TesteCalculadora</strong></p>
+ * 
+ * <p>Esta classe realiza os testes com as operações matemáticas, para que o sistema funcione corretamente
+ * e trate todas as exceções, para que o usuário consiga realizar todos os calculos que deseja.</p>
+ * <p>Esse Projeto é utilizado para realizar:</p>
+ * <ul>
+ * 		<li>Técnicas de revisão estático (caixa branca)</li>
+ * 		<li>Técnicas de revisão (caixa branca)</li>
+ * 		<li>Aplicação de refatoração em código java</li>
+ * 		<li>Documentação com javadoc</li>
+ * 
+ * </ul>
+ *  *<p>Após a refatoração, os métodos da classe Calculadora, tornaram-se <strong>métodos puros</strong>,
+ *  sem efeitos colaterais, permitindo maior legibilidade e facilitando testes unitários.</p>
+ *  
+ *  @author Luiz Eduardo
+ *  @version 1.0
+ */
 package calculadora;
 
 public class TesteCalculadora {
@@ -5,12 +24,29 @@ public class TesteCalculadora {
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		
-		// Testes Rapidos
-		calc.calc(2, 3, "+"); // esperado: 5
-		calc.calc(10, 4, "-"); // esperado: 6
-		calc.calc(3, 5, "*"); // esperado: 15
-		calc.calc(8, 2, "/"); // esperado: 4
-		calc.calc(8, 0, "/"); // divisão por zero
-		calc.calc(5, 5, "x"); // operação invalida
+		/**
+		 * Print no console inserindo novos objetos com os valores e operador matemático, para a realização do cálculo
+		 * 
+		 * @System.out.println os valores são inseridos, logo em seguida o operador matemático
+		 * @try realiza os testes de exceções
+		 * @catch informa erro caso caia em exceção
+		 */
+		
+		System.out.println(calc.calcular(2, 3, "+"));
+		System.out.println(calc.calcular(10, 4, "-"));
+		System.out.println(calc.calcular(3, 5, "*"));
+		System.out.println(calc.calcular(8, 2, "/"));
+		
+		try {
+			System.out.println(calc.calcular(8, 0, "/")); // exceção
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		try {
+			System.out.println(calc.calcular(5, 5, "x")); // exceção
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
